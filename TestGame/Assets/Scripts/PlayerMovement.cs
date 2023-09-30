3870fe999d89fdf9de08f0e1c146b0b86a32ab4f
TestyEldrich <olzhas2106@gmail.com>
2023-09-30 19:51:53 +0600

    Added cinemachine virtual camera and done basic player movement
    Now player can move in 4 directions with joystick on the screen and has its collision to some buildings.

--- TestGame/Assets/Scripts/PlayerMovement.cs
+++ TestGame/Assets/Scripts/PlayerMovement.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Android;
+using UnityEngine.InputSystem.Controls;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Joystick joystick;
+    public float moveSpeed;
+
+    float hInput, vInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +22,11 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void FixedUpdate() {
+        hInput = joystick.Horizontal * moveSpeed;
+        vInput = joystick.Vertical * moveSpeed;
+        transform.Translate(hInput, vInput, 0);
     }
 }
