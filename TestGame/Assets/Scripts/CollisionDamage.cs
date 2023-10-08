f55a0390dd84d5a6efb5891eed377b95ea5f6098
TestyEldrich <olzhas2106@gmail.com>
2023-10-08 17:22:30 +0600

    Zombie is now able to knockback the player to some distance after dealing damage

--- TestGame/Assets/Scripts/CollisionDamage.cs
+++ TestGame/Assets/Scripts/CollisionDamage.cs
@@ -2,19 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem.Android;
 
 public class CollisionDamage : MonoBehaviour
 {
     public float collisionDamage;
     public string collisionTag;
-    public float pushForce;
+    public float pushForce, delay;
+    private Rigidbody2D rB2d;
+
+    public UnityEvent OnBegin, OnDone;
 
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.tag == collisionTag) {
             Health health = collision.gameObject.GetComponent<Health>();
             health.Damage(collisionDamage);
-            
+
+            StopAllCoroutines();
+            OnBegin?.Invoke();
+            rB2d = collision.gameObject.GetComponent<Rigidbody2D>();
+            var direction = transform.position - collision.transform.position;
+            direction.Normalize();
+
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(-direction * pushForce, ForceMode2D.Impulse);
+            StartCoroutine(Reset());
         }
+    }
+
+    private IEnumerator Reset() {
+        yield return new WaitForSeconds(delay);
+        rB2d.velocity = Vector3.zero;
+        OnBegin?.Invoke();
     }
 }
